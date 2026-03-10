bc5ef6e98e79e498cfe975fa82dad55c7d59719e
Alex <Rieznik.O.A@nmu.one>
2026-03-10 02:54:37 +0200

    Solution#4

--- db/models.py
+++ db/models.py
@@ -35,7 +35,7 @@ class CinemaHall(models.Model):
         return self.name
 
     @property
-    def capacity(self):
+    def capacity(self) -> int:
         return self.rows * self.seats_in_row
 
 
